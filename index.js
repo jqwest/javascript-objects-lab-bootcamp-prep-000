a021b265a48b8d602136c603c4cf9528f9965772
john L <johnleavell@gmail.com>
2019-09-09 02:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var recipes = {}
 // returns an object with the origal key value pairs and the new key value pair
 //expected [function; updateObjectWithKeyAndValue] to match { prop: 1, prop2: 2}
 function updateObjectWithKeyAndValue(obj, key, value) {
+  var obj = { prop: 1 }
 return Object.assign({}, obj, {[key]: value})
 }
 
