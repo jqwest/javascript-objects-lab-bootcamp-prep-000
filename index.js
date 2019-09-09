def981e4b7f9f589f2a441434b0565a4dcc3658b
john L <johnleavell@gmail.com>
2019-09-09 02:32:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ var recipes = {}
 // returns an object with the origal key value pairs and the new key value pair
 //expected [function; updateObjectWithKeyAndValue] to match { prop: 1, prop2: 2}
 function updateObjectWithKeyAndValue(obj, key, value) {
-
 return Object.assign({}, obj, {[key]: value})
 }
 
 
-
-
 function deleteFromObjectByKey(obj, key, value) {
-
+delete key.value
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
